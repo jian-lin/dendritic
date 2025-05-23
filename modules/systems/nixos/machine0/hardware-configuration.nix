078b8d8412f928f8151019f945699698b83ffbfd
Lin Jian <me@linj.tech>
2025-05-23 10:14:47 +0800

    Make nix flake check happy by adding dummy hardware config

--- modules/systems/nixos/machine0/hardware-configuration.nix
+++ modules/systems/nixos/machine0/hardware-configuration.nix
@@ -1,6 +1,12 @@
 {
   my.modules.nixos.machine0 = {
     nixpkgs.hostPlatform = "x86_64-linux";
-    # FIXME copy content of your hardware-configuration.nix here
+
+    # dummy hardware config to make "nix flake check" happy
+    # FIXME replace this with content of your hardware-configuration.nix
+    fileSystems."/" = {
+      device = "/dev/FIXME";
+    };
+    boot.loader.grub.device = "nodev";
   };
 }
